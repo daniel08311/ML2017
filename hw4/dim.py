3c15cd41449a1dfa16db3d6487ac897389d6ff79
daniel08311 <b03705013@ntu.edu.tw>
2017-05-14 16:04:08 +0800

    dim.sh, dim.py, model uplaod

---
+++ hw4/dim.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sklearn import preprocessing
+from keras.models import load_model
+import csv
+import keras,sys
+from keras import backend as K
+from sklearn import preprocessing
+
+K.set_image_data_format("channels_first")
+K.set_image_dim_ordering("th")
+
+npz = sys.argv[1]
+output = sys.argv[2]
+
+model = load_model("model.h5")
+data = np.load(npz)
+X_test = np.zeros((200,200))
+for i in range(200):
+    x = data[str(i)]
+    mean = np.mean(x , axis=0).reshape((1,100))
+    var = np.var(x , axis=0).reshape((1,100))
+    col = np.concatenate((var,mean),axis = 1)
+    X_test[i] = col
+X_test = preprocessing.scale(X_test)
+predict = model.predict(X_test)
+predict = np.log(predict)
+csv = open(output,"w")
+csv.write("SetId,LogDim\n")
+for i in range(len(predict)):
+    csv.write(str(i) + "," + str(predict[i][0]) + "\n")
+csv.close()
